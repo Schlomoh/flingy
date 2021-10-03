5ccc49a690c59e0d1d21d54a11322dd3240ecf2f
Schlomoh <71161651+Schlomoh@users.noreply.github.com>
2021-10-03 14:58:30 +0200

    add adult filter to texts

--- src/components/tools/get_starter.tsx
+++ src/components/tools/get_starter.tsx
@@ -1,8 +1,64 @@
 import * as lines from "../../assets/all_lines.json";
 
-export const getStarter = (category: string) => {
+export const getStarter = (category: any) => {
   const file: any = lines;
   const selection = file.default[category.toString()];
-  let line = selection[Math.floor(Math.random() * selection.length)];
-  return line;
+  let line = selection[Math.round(Math.random() * selection.length)];
+  return {
+    message: line,
+    rValue: (55 + 40 * Math.random()).toFixed(1),
+    shown: false
+  };
+};
+
+export const checkMessage = (message: string) => {
+  const xcluders = [
+    "butt",
+    "penis",
+    "dick",
+    "pussy",
+    "fuck",
+    "sex",
+    "ass",
+    "bang",
+    "sexual",
+    "hot",
+    "hotter",
+    "sexy",
+    "knees",
+    "love",
+    "erect",
+    "erected",
+    "nipple",
+    "nipples",
+    "boobs",
+    "tits",
+    "ram",
+    "babe",
+    "baby",
+    "d",
+    "naked",
+    "nude",
+    "nudes",
+    "hit",
+  ];
+
+  let appropriate = true;
+  const items = message
+    .toLowerCase()
+    .replaceAll("?", "")
+    .replaceAll(":", "")
+    .replaceAll(";", "")
+    .replaceAll('"', "")
+    .replaceAll(".", "")
+    .replaceAll(",", "")
+    .split(" ");
+  for (let i in items) {
+    for (let x in xcluders) {
+      if (x === i) {
+        appropriate = false;
+      }
+    }
+  }
+  return appropriate;
 };
