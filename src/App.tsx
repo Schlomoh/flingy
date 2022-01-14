bc8b0730bdf920e7d5c5c8be473b917797d2b86a
Schlomoh <moritz@weltfern.com>
2022-01-14 19:23:28 +0100

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,71 +1,12 @@
-import { useEffect, useState } from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
 
-// Pages
-import { Home } from "./pages/Home";
-// import { Reply } from "./pages/Reply";
-import { Starter } from "./pages/Starter";
-import { Privacy } from "./pages/Privacy";
-
-// Components
-import { OL } from "./components/GlobalComponents";
-import { StBase } from "./components/StyledComps";
-import { CookieConsentBox } from "./components/cookieConsent/cookieConsent";
-import { getCookie, setCookie } from "./components/cookieConsent/manageCookies";
-
-export interface ICookiesObj {
-  [index: string]: {
-    show: Boolean;
-    necessary: Boolean;
-    analytics: Boolean;
-    advertisement: Boolean;
-  };
-}
-
-function App(): JSX.Element {
-  const [nsfw, setNsfw] = useState(false);
-  const [consent, setConsent]: any = useState({
-    show: true,
-    necessary: true,
-    analytics: false,
-    advertisement: false,
-  });
-
-  useEffect(() => {
-    let inc = document.cookie.includes("selection");
-    if (consent.show && inc) setConsent(getCookie(consent));
-    else if (!consent.show && !inc) {
-      setCookie(consent);
-    }
-  }, [consent, setConsent]);
-
+const App: React.FC = (): JSX.Element => {
   return (
-    <div className="App">
-      <StBase>
-        <Router>
-          <CookieConsentBox consent={[consent, setConsent]} />
-          <Switch>
-            <Route path="/starter">
-              <OL nsfw={nsfw}>
-                <Starter nsfw={{ get: nsfw, set: setNsfw }} />
-              </OL>
-            </Route>
-            {/* <Route path="/reply">
-            <OL>
-            <Reply />
-            </OL>
-          </Route> */}
-            <Route path="/privacy-policy">
-              <Privacy />
-            </Route>
-            <Route path="/" exact>
-              <Home />
-            </Route>
-          </Switch>
-        </Router>
-      </StBase>
-    </div>
+    <Router>
+      <Home />
+    </Router>
   );
-}
+};
 
 export default App;
