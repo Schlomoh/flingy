3fd906d313e87e3b46afc15255440524d0cf78b8
Schlomoh <moritz@weltfern.com>
2022-01-20 01:01:13 +0100

    add comments

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,11 @@
+// routing
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+// theming
 import { ThemeProvider } from "styled-components";
 import theme from "./components/styleComponents/theme";
+
+// pages
 import Home from "./pages/Home";
 import PickupPage from "./pages/PickupPage";
 
